918ac81e6f319bb8a2b31319aa627d692cb00c4e
Valerii Kalenyuk <vakabo@gmail.com>
2021-10-24 03:55:38 +0300

    second task done

--- src/index.js
+++ src/index.js
@@ -9,3 +9,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// function randomColor() {
+//   // const color = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+//   const color = Math.floor(
+//     Math.random() * (16777215 - 1000000) + 1000000
+//   ).toString(16);
+//   console.log(color);
+// }
+
+// randomColor();
